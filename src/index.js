7e4af73293570c662b2eab9fc8bcb5e32d970c50
Konrad Jaworski <konradx530@gmail.com>
2020-01-06 23:54:10 +0100

    pierwszy season

--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
-import ApprovalCard from './ApprovalCard'
+import React from "react";
+import ReactDOM from "react-dom";
 
 
 const App = () => {
-    return (
-        <div className="ui container comments">
-            <ApprovalCard >
-            <CommentDetail 
-            author="edek"
-            timeAgo="Today at 4:30PM" 
-            textComment="Best regards"
-            avatar={faker.image.avatar()} 
-            />
-            </ApprovalCard >
-            <CommentDetail 
-            author="zbychu dCi" 
-            timeAgo="Today at 2:00AM" 
-            textComment="everybody" 
-            avatar={faker.image.avatar()}
-            />
-            <CommentDetail 
-            author="wiesiek dTi" 
-            timeAgo="Yesteday at 5:00PM" 
-            textComment="Hello React!" 
-            avatar={faker.image.avatar()}
-            />
-        </div>
-
-    );
+    return <div>Hi there! </div>
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'))
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+

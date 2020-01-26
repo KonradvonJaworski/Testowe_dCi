ddc9b233491823f14a73cbd3a29c7ab2a63f661a
Konrad Jaworski <konradx530@gmail.com>
2020-01-26 18:45:57 +0100

    klasowe komponenty

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-const App = () => {
-    return <div>Hi there! </div>
-};
+    this.state = { lat: null };
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => console.log(err)
+    );
+  }
 
+  render() {
+    return <div>Latitude: {this.state.lat} </div>;
+  }
+}
 
+ReactDOM.render(<App />, document.querySelector('#root'));
